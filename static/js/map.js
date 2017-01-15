a0eaab2ede10cb5433a3ce85ddbc2a038f85f8be
Rook25 <rookster@gmail.com>
2017-01-16 08:49:08 +1300

    Remove popups on mouseover for mobile/touch devices

--- static/js/map.js
+++ static/js/map.js
@@ -621,11 +621,13 @@ function setupGymMarker (item) {
       marker.persist = null
     })
 
-    marker.addListener('mouseover', function () {
-      marker.infoWindow.open(map, marker)
-      clearSelection()
-      updateLabelDiffTime()
-    })
+	if (!isMobileDevice() && !isTouchDevice()) {
+      marker.addListener('mouseover', function () {
+        marker.infoWindow.open(map, marker)
+        clearSelection()
+        updateLabelDiffTime()
+      })
+    }
 
     marker.addListener('mouseout', function () {
       if (!marker.persist) {
@@ -815,11 +817,13 @@ function addListeners (marker) {
     marker.persist = null
   })
 
-  marker.addListener('mouseover', function () {
-    marker.infoWindow.open(map, marker)
-    clearSelection()
-    updateLabelDiffTime()
-  })
+  if (!isMobileDevice() && !isTouchDevice()) {
+    marker.addListener('mouseover', function () {
+      marker.infoWindow.open(map, marker)
+      clearSelection()
+      updateLabelDiffTime()
+    })
+  }
 
   marker.addListener('mouseout', function () {
     if (!marker.persist) {
