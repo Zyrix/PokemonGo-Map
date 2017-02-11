b4399faf0737be7f0d8a56c9a433d317f51b1d14
Philipp Brandl <pbrandl@inovex.de>
2017-02-11 06:33:48 +0100

    another small fix for new icons

--- static/js/map.common.js
+++ static/js/map.common.js
@@ -1052,8 +1052,8 @@ function getGoogleSprite (index, sprite, displayHeight) {
   // Crop icon just a tiny bit to avoid bleedover from neighbor
   var scaledIconSize = new google.maps.Size(scale * sprite.iconWidth - 1, scale * sprite.iconHeight - 1)
   var scaledIconOffset = new google.maps.Point(
-    (index % sprite.columns) * sprite.iconWidth * scale + 0.5,
-    Math.floor(index / sprite.columns) * sprite.iconHeight * scale + 0.5)
+    (index % sprite.columns) * sprite.iconWidth * scale + 1,
+    Math.floor(index / sprite.columns) * sprite.iconHeight * scale + 1)
   var scaledSpriteSize = new google.maps.Size(scale * sprite.spriteWidth, scale * sprite.spriteHeight)
   var scaledIconCenterOffset = new google.maps.Point(scale * sprite.iconWidth / 2, scale * sprite.iconHeight / 2)
 
